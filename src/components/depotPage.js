a384ee4503e5537749923e288208bdc17894a41f
Eskil Brännerud <eskbr129@student.liu.se>
2022-04-10 13:51:00 +0200

    reformat

--- src/components/depotPage.js
+++ src/components/depotPage.js
@@ -3,7 +3,7 @@ import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import {internalIpV6, internalIpV4} from 'internal-ip';
+import { internalIpV6, internalIpV4 } from 'internal-ip';
 import StopWatch from './stopwatch';
 
 import PouchDB from 'pouchdb';
@@ -19,132 +19,128 @@ class DepotPage extends React.Component {
 
     this.state = {
       remoteDB: new DB('http://localhost:5984/myremotedb'),
-      settingsDB: new DB("SettingsDB"),
-      localIp : "N/A",
-      activeRace : 0,
-      raceData: {"largeKart":"4","smallKart":"2","doubleKart":"0"},
-      statsData: {"nextRace":"1","nrOfRaceQueue":"2","queueTime":"3"},
-      ioStats: {"timeSinceDbConnection":0,"timeSinceButtonPress":0},
+      useKeyboard: false,
+      localIp: "N/A",
+      activeRace: 0,
+      raceData: { "largeKart": "4", "smallKart": "2", "doubleKart": "0" },
+      statsData: { "nextRace": "1", "nrOfRaceQueue": "2", "queueTime": "3" },
+      ioStats: { "timeSinceDbConnection": 0, "timeSinceButtonPress": 0 },
+    };
+
+    this.keyEventFunction = this.keyEventFunction.bind(this);
   };
 
-  this.keyEventFunction = this.keyEventFunction.bind(this);
-  };
-
-  async componentDidMount(){
+  async componentDidMount() {
     document.addEventListener("keydown", this.keyEventFunction, false);
-    const ip = await internalIpV4()  
+    const ip = await internalIpV4()
     console.log(ip)
     this.setState({
-        localIp : ip
+      localIp: ip
     })
     var allRaceData = await this.state.remoteDB.getRaceDataDB();
-    this.updateRaceData(allRaceData,this.state.activeRace);
+    this.updateRaceData(allRaceData, this.state.activeRace);
 
     const self = this;
-    
+
     var changes = this.state.remoteDB.db.changes({
       since: 'now',
       live: true,
       include_docs: true
-    }).on('change', async function(change) {
+    }).on('change', async function (change) {
       // handle change
       console.log("changes")
       var allRaceData = await self.state.remoteDB.getRaceDataDB();
       var fetchedCurrentRaceNr = await self.state.remoteDB.getCurrentRaceNrDB();
       console.log(fetchedCurrentRaceNr);
-      self.setState({activeRace:fetchedCurrentRaceNr})
-      self.updateRaceData(allRaceData,self.state.activeRace);
-    }).on('complete', function(info) {
+      self.setState({ activeRace: fetchedCurrentRaceNr })
+      self.updateRaceData(allRaceData, self.state.activeRace);
+    }).on('complete', function (info) {
       // changes() was canceled
     }).on('error', function (err) {
       console.log(err);
     });
   };
 
-  componentWillUnmount(){
+  componentWillUnmount() {
     document.removeEventListener("keydown", this.keyEventFunction, false);
   };
 
-  updateRaceData(raceData,activeRace){
-    console.log(raceData)
-    console.log(activeRace)
-    if(activeRace == raceData.length){
+  updateRaceData(raceData, activeRace) {
+    if (activeRace == raceData.length) {
       this.setState({
-        raceData: {"largeKart":"⌛","smallKart":"⌛","doubleKart":"⌛"}
+        raceData: { "largeKart": "⌛", "smallKart": "⌛", "doubleKart": "⌛" }
       })
     }
-    else{
+    else {
+      this.setState({
+        raceData: { "largeKart": raceData[activeRace]["largeKart"], "smallKart": raceData[activeRace]["smallKart"], "doubleKart": raceData[activeRace]["doubleKart"] }
+      })
+    }
     this.setState({
-      raceData: {"largeKart":raceData[activeRace]["largeKart"],"smallKart":raceData[activeRace]["smallKart"],"doubleKart":raceData[activeRace]["doubleKart"]}
+      statsData: { "nextRace": activeRace + 1, "nrOfRaceQueue": raceData.length - activeRace, "queueTime": (raceData.length - activeRace) * 8 },
     })
-  }
-  this.setState({
-    statsData: {"nextRace":activeRace+1,"nrOfRaceQueue":raceData.length-activeRace,"queueTime":(raceData.length-activeRace)*8},
-  })
 
   }
 
-  async keyEventFunction(event){
+  async keyEventFunction(event) {
+    if (!this.state.useKeyboard) { return }
     var allRaceData = await this.state.remoteDB.getRaceDataDB();
-    //console.log(this.state.raceData);
-    
-    if(event.keyCode === 39) {
-        console.log("höger")
-        if(this.state.activeRace<allRaceData.length){
+
+    if (event.keyCode === 39) {
+      if (this.state.activeRace < allRaceData.length) {
         this.setState({
-          activeRace: (this.state.activeRace+1)
-      })
+          activeRace: (this.state.activeRace + 1)
+        })
+      }
+    } else if (event.keyCode === 37) {
+      if (this.state.activeRace > 0) {
+        this.setState({
+          activeRace: (this.state.activeRace - 1)
+        })
+      }
     }
-    }else if(event.keyCode === 37){
-        console.log("vänster")
-        if(this.state.activeRace>0){
-            this.setState({
-              activeRace: (this.state.activeRace-1)
-          })
-        }
-    }
-    this.updateRaceData(allRaceData,this.state.activeRace);
+    this.updateRaceData(allRaceData, this.state.activeRace);
   };
 
 
 
   render() {
-      
+
 
     return (
       <Container className="app bg-light shadow-5-strong" >
         <Row className="justify-content-md-center">
-          <Col className="text-center" id="largeKartOutput" style={{fontSize: "7vh"}}>Depå</Col>
+          <Col className="text-center" id="largeKartOutput" style={{ fontSize: "7vh" }}>Depå</Col>
         </Row>
         <Row className="justify-content-md-center">
-          <Col className="text-center" id="largeKartOutput" style={{fontSize: "7vh"}}>Nästa Race: {this.state.activeRace+1}</Col>
-          <Col className="text-center" id="largeKartOutput" style={{fontSize: "7vh"}}>Stora: {this.state.raceData.largeKart}</Col>
+          <Col className="text-center" id="largeKartOutput" style={{ fontSize: "7vh" }}>Nästa Race: {this.state.activeRace + 1}</Col>
+          <Col className="text-center" id="largeKartOutput" style={{ fontSize: "7vh" }}>Stora: {this.state.raceData.largeKart}</Col>
         </Row>
         <Row className="justify-content-md-center" >
-            <Col className="d-grid" md="8" style={{height: "10vh"}}>
-            </Col>
+          <Col className="d-grid" md="8" style={{ height: "10vh" }}>
+          </Col>
         </Row>
         <Row className="justify-content-md-center">
-          <Col className="text-center" id="smallKartOutput" style={{fontSize: "7vh"}}>Race kvar: {this.state.statsData.nrOfRaceQueue}</Col>
-          <Col className="text-center" id="doubleKartOutput" style={{fontSize: "7vh"}}>Små: {this.state.raceData.smallKart}</Col>
+          <Col className="text-center" id="smallKartOutput" style={{ fontSize: "7vh" }}>Race kvar: {this.state.statsData.nrOfRaceQueue}</Col>
+          <Col className="text-center" id="doubleKartOutput" style={{ fontSize: "7vh" }}>Små: {this.state.raceData.smallKart}</Col>
         </Row>
         <Row className="justify-content-md-center" >
-            <Col className="d-grid" md="8" style={{height: "10vh"}}>
-            </Col>
+          <Col className="d-grid" md="8" style={{ height: "10vh" }}>
+          </Col>
         </Row>
         <Row className="justify-content-md-center">
-        <Col className="text-center" id="smallKartOutput" style={{fontSize: "7vh"}}>Kötid: {this.state.statsData.queueTime}min</Col>
-          <Col className="text-center" id="doubleKartOutput" style={{fontSize: "7vh"}}>Dubbla: {this.state.raceData.doubleKart}</Col>
+          <Col className="text-center" id="smallKartOutput" style={{ fontSize: "7vh" }}>Kötid: {this.state.statsData.queueTime}min</Col>
+          <Col className="text-center" id="doubleKartOutput" style={{ fontSize: "7vh" }}>Dubbla: {this.state.raceData.doubleKart}</Col>
         </Row>
         <Row className="justify-content-md-center" >
-            <Col className="d-grid" md="8" style={{height: "20vh"}}>
-            </Col>
+          <Col className="d-grid" md="8" style={{ height: "20vh" }}>
+          </Col>
         </Row>
         <Row className="justify-content-md-center">
-          <Col className="text-center" style={{fontSize: "2vh"}}>IP-{">"} {this.state.localIp}</Col>
-          <Col className="text-center" style={{fontSize: "2vh"}}>Senaste input-{">"} <StopWatch/></Col>
-          <Col className="text-center" style={{fontSize: "2vh"}}>Senaste query-{">"} {this.state.ioStats.timeSinceDbConnection}s</Col> 
-          <Col className="text-center" style={{fontSize: "2vh"}}>Made by-{">"} Eskil Brännerud</Col>
+          <Col className="text-center" style={{ fontSize: "2vh" }}>IP-{">"} {this.state.localIp}</Col>
+          <Col className="text-center" style={{ fontSize: "2vh" }}>Senaste input-{">"} <StopWatch /></Col>
+          <Col className="text-center" style={{ fontSize: "2vh" }}>Senaste query-{">"} {this.state.ioStats.timeSinceDbConnection}s</Col>
+          <Col className="text-center" style={{ fontSize: "2vh" }}>Made by-{">"} Eskil Brännerud</Col>
         </Row>
       </Container>
     );
--- src/components/stopwatch.js
+++ src/components/stopwatch.js
@@ -1,78 +1,76 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
 class StopWatch extends React.Component {
-    constructor() {
-      super();
-      this.state = { time: {}, seconds: 0 };
-      this.timer = 0;
-      this.startTimer = this.startTimer.bind(this);
-      this.resetTimer = this.resetTimer.bind(this);
-      this.countUp = this.countUp.bind(this);
-      this.keyEventFunction = this.keyEventFunction.bind(this)
-    }
-  
-    secondsToTime(secs){
-      let hours = Math.floor(secs / (60 * 60));
-  
-      let divisor_for_minutes = secs % (60 * 60);
-      let minutes = Math.floor(divisor_for_minutes / 60);
-  
-      let divisor_for_seconds = divisor_for_minutes % 60;
-      let seconds = Math.ceil(divisor_for_seconds);
-  
-      let obj = {
-        "h": hours,
-        "m": minutes,
-        "s": seconds
-      };
-      return obj;
-    }
-  
-    componentDidMount() {
-      document.addEventListener("keydown", this.keyEventFunction, false);
-      let timeLeftVar = this.secondsToTime(this.state.seconds);
-      this.setState({ time: timeLeftVar });
-      this.startTimer();
-    }
-
-    componentWillUnmount(){
-      document.removeEventListener("keydown", this.keyEventFunction, false);
-      clearInterval(this.timer);
-    };
-  
-    startTimer() {
-        this.timer = setInterval(this.countUp, 1000);
-    };
-    resetTimer() {
-      console.log("lol")
-        this.setState({seconds: -1});
-        console.log("gay")
-    };
-
-    keyEventFunction(event){
-      if(event.keyCode === 39) {
-        this.resetTimer()
-      }else if(event.keyCode === 37){
-        this.resetTimer()
-      };
-    };
-  
-    countUp() {
-      // Remove one second, set state so a re-render happens.
-      let seconds = this.state.seconds + 1;
-      this.setState({
-        time: this.secondsToTime(seconds),
-        seconds: seconds,
-      }); 
-    }
-  
-    render() {
-      return(
-         <Container> 
-           {this.state.time.m}:{this.state.time.s}
-         </Container>
-      );
-    }
+  constructor() {
+    super();
+    this.state = { time: {}, seconds: 0 };
+    this.timer = 0;
+    this.startTimer = this.startTimer.bind(this);
+    this.resetTimer = this.resetTimer.bind(this);
+    this.countUp = this.countUp.bind(this);
+    this.keyEventFunction = this.keyEventFunction.bind(this)
   }
 
-  export default StopWatch;
+  secondsToTime(secs) {
+    let hours = Math.floor(secs / (60 * 60));
+
+    let divisor_for_minutes = secs % (60 * 60);
+    let minutes = Math.floor(divisor_for_minutes / 60);
+
+    let divisor_for_seconds = divisor_for_minutes % 60;
+    let seconds = Math.ceil(divisor_for_seconds);
+
+    let obj = {
+      "h": hours,
+      "m": minutes,
+      "s": seconds
+    };
+    return obj;
+  }
+
+  componentDidMount() {
+    document.addEventListener("keydown", this.keyEventFunction, false);
+    let timeLeftVar = this.secondsToTime(this.state.seconds);
+    this.setState({ time: timeLeftVar });
+    this.startTimer();
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener("keydown", this.keyEventFunction, false);
+    clearInterval(this.timer);
+  };
+
+  startTimer() {
+    this.timer = setInterval(this.countUp, 1000);
+  };
+  resetTimer() {
+    this.setState({ seconds: -1 });
+  };
+
+  keyEventFunction(event) {
+    if (event.keyCode === 39) {
+      this.resetTimer()
+    } else if (event.keyCode === 37) {
+      this.resetTimer()
+    };
+  };
+
+  countUp() {
+    // Remove one second, set state so a re-render happens.
+    let seconds = this.state.seconds + 1;
+    this.setState({
+      time: this.secondsToTime(seconds),
+      seconds: seconds,
+    });
+  }
+
+  render() {
+    return (
+      <Container>
+        {this.state.time.m}:{this.state.time.s}
+      </Container>
+    );
+  }
+}
+
+export default StopWatch;
